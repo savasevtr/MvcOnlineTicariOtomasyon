26f032438257dad1f62291b89e1581b8146c0929
savasevtr <savas.ev@yahoo.com>
2020-09-20 17:33:37 +0300

    update

--- MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebGrease.Css.Ast.Selectors;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
@@ -33,6 +34,37 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             }
 
             context.Faturas.Add(fatura);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var fatura = context.Faturas.Find(id);
+
+            return View(fatura);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Fatura fatura)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(fatura);
+            }
+
+            var _fatura = context.Faturas.Find(fatura.FaturaID);
+
+            _fatura.FaturaSeriNo = fatura.FaturaSeriNo;
+            _fatura.FaturaSiraNo = fatura.FaturaSiraNo;
+            _fatura.Tarih = fatura.Tarih;
+            _fatura.Saat = fatura.Saat;
+            _fatura.TeslimEden = fatura.TeslimEden;
+            _fatura.TeslimAlan = fatura.TeslimAlan;
+            _fatura.VergiDairesi = fatura.VergiDairesi;
+
             context.SaveChanges();
 
             return RedirectToAction("Index");
