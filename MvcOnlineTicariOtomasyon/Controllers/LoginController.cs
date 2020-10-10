a5b70dfe9e84a9be77c588ff60bc0722400112b9
savasevtr <savas.ev@yahoo.com>
2020-10-10 23:16:16 +0300

    update

--- MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class LoginController : Controller
     {
+        Context context = new Context();
+
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public PartialViewResult CariKayitPartial()
         {
+            return PartialView();
+        }
+        
+        [HttpPost]
+        public PartialViewResult CariKayitPartial(Cari cari)
+        {
+            context.Caris.Add(cari);
+            context.SaveChanges();
+
             return PartialView();
         }
     }
