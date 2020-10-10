0dd854755bd2bf3a091afcc14ca3491e5b022860
savasevtr <savas.ev@yahoo.com>
2020-10-10 23:58:50 +0300

    update

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariPanelController : Controller
+    {
+
+        Context context = new Context();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
