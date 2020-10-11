a3652a052a7358bae5adc85c0d6a57fbee215d0d
savasevtr <savas.ev@yahoo.com>
2020-10-11 15:49:12 +0300

    update

--- MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -7,19 +7,35 @@ using MvcOnlineTicariOtomasyon.Models;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
+    [Authorize]
     public class CariPanelController : Controller
     {
 
         Context context = new Context();
-
-        [Authorize]
+  
         public ActionResult Index()
         {
             var mail = (string)Session["CariMail"];
             var cari = context.Caris.FirstOrDefault(x => x.CariMail == mail);
-            ViewBag.cari = cari;
 
-            return View();
+            return View(cari);
         }
+
+        [HttpPost]
+        public ActionResult Index(Cari cari)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Siparislerim()
+        {
+            var mail = (string)Session["CariMail"];
+            var id = context.Caris.Where(x => x.CariMail == mail).Select(y => y.CariID).FirstOrDefault();
+
+            var siparisler = context.SatisHarekets.Where(x => x.CariID == id).ToList();
+
+            return View(siparisler);
+        }
+
     }
 }
