625d6716fd3a012d8ff4149dfaf276a38d90023c
savasevtr <savas.ev@yahoo.com>
2020-10-12 22:17:30 +0300

    update

---
+++ MvcOnlineTicariOtomasyon/Controllers/KargoController.cs
@@ -0,0 +1,35 @@
+﻿using MvcOnlineTicariOtomasyon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class KargoController : Controller
+    {
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var kargolar = context.KargoDetays.ToList();
+
+            return View(kargolar);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(KargoDetay kargoDetay)
+        {
+            context.KargoDetays.Add(kargoDetay);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
