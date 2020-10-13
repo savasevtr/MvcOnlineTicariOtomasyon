2e115bc069a0c44bcb774551bf3a2a9107eff088
savasevtr <savas.ev@yahoo.com>
2020-10-13 22:51:14 +0300

    update

--- MvcOnlineTicariOtomasyon/Controllers/KargoController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/KargoController.cs
@@ -10,16 +10,34 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     public class KargoController : Controller
     {
         Context context = new Context();
-        public ActionResult Index()
+        public ActionResult Index(string p)
         {
-            var kargolar = context.KargoDetays.ToList();
+            var kargolar = from x in context.KargoDetays select x;
 
-            return View(kargolar);
+            if (!string.IsNullOrEmpty(p))
+            {
+                kargolar = kargolar.Where(y => y.TakipKodu.Contains(p));
+            }
+
+            return View(kargolar.ToList());
         }
 
         [HttpGet]
         public ActionResult Create()
         {
+            Random random = new Random();
+            string[] karakterler = { "A", "B", "C", "D", "E", "F", "G", "H", "K" };
+            int k1, k2, k3;
+            k1 = random.Next(0, karakterler.Length);
+            k2 = random.Next(0, karakterler.Length);
+            k3 = random.Next(0, karakterler.Length);
+            int s1, s2, s3;
+            s1 = random.Next(100, 1000);
+            s2 = random.Next(10, 100);
+            s3 = random.Next(10, 100);
+            string kod = s1.ToString() + karakterler[k1] + s2 + karakterler[k2] + s3 + karakterler[k3];
+            ViewBag.takipkodu = kod;
+
             return View();
         }
 
@@ -30,6 +48,13 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             context.SaveChanges();
 
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(string id)
+        {
+            var detaylar = context.KargoTakips.Where(x => x.TakipKodu == id).ToList();
+
+            return View(detaylar);
         }
     }
 }
